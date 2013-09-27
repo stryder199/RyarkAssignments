0a694beea6cf90c4d2b761a9109a6363056be3c2
Ryan McDonald <ryan.mcdonald8@gmail.com>
2013-09-26 20:32:23 -0700

    Added a test module.

---
+++ Assignment1/crypto_test.py
@@ -0,0 +1,61 @@
+'''
+Created on Sep 26, 2013
+
+@author: Ryan
+'''
+
+import unittest
+from crypto import *
+
+class TestCryptoFunctions(unittest.TestCase):
+    
+    def test_caesar_encrypt(self):
+        self.assertEqual(caeser_encrypt('HI', 0), 'HI')
+        self.assertEqual(caeser_encrypt('HI', 26), 'HI')
+        
+        self.assertEqual(caeser_encrypt('HI', 1), 'IJ')
+        
+    def test_caesar_decrypt(self):
+        self.assertEqual(caeser_decrypt('HI', 0), 'HI')
+        self.assertEqual(caeser_decrypt('HI', 26), 'HI')
+        
+        self.assertEqual(caeser_decrypt('HI', 1), 'GH')
+        
+    def test_substitution_encrypt(self):
+        pass
+    
+    def test_substitution_decrypt(self):
+        pass
+    
+    def test_vernam_encrypt(self):
+        pass
+    
+    def test_vernam_decrypt(self):
+        pass
+    
+    def test_book_encrypt(self):
+        pass
+    
+    def test_book_decrypt(self):
+        pass
+    
+    def test_columnar_encrypt(self):
+        pass
+    
+    def test_columnar_decrypt(self):
+        pass
+    
+    def test_rsa_encrypt(self):
+        pass
+    
+    def test_rsa_decrypt(self):
+        pass
+    
+    def test_count_letters(self):
+        pass
+    
+    def test_count_digrams(self):
+        pass
+    
+if __name__ == '__main__':
+    unittest.main()
