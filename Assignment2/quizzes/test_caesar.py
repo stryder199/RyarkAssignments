5b59dbf5ac7aae5720b35580409e435bd41959af
Mark Lessard <lessardme@hotmail.com>
2013-11-06 11:15:57 -0800

    added a few more tests

--- Assignment2/quizzes/test_caesar.py
+++ Assignment2/quizzes/test_caesar.py
@@ -2,6 +2,8 @@ question_list = [
 #	(mark, 	count, [directories])
 	(1,	1,	['test_caesar/test_0']),
 	(1,	1,	['test_caesar/test_1']),
+	(1,	1,	['test_caesar/test_2']),
+	(1,	1,	['test_caesar/test_3']),
 ]
 practice_mode = True
 standalone = False
--- Assignment2/testing_review/caesar/templates/template.py
+++ Assignment2/testing_review/caesar/templates/template.py
@@ -3,6 +3,10 @@ question_groups = [
 		  #plaintext  #key #hotspots
 		[ 'plaintexta', 1, [0, 1, 2] ],
 
-		[ 'plaintextb', 2, [0, 1, 2] ]
+		[ 'plaintextb', 2, [0, 1, 2] ],
+		
+		[ 'ilovecsc', 0, [3, 4, 7] ],
+		
+		[ 'ilovecsc', 25, [5, 6, 7] ]
 	],
 ]
